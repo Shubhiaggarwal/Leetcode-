81947bffae513a4c75eb58f6d3c91c185700cdce
shubhi agarwal <shubhi00400@gmail.com>
2026-07-24 23:29:19 +0530

    Time: 0 ms (100%), Space: 40.2 MB (81.58%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int zero = 0;
+        int zeroes = 0;
+        int n = nums.size();
+        vector<int> res(n);
         int prod = 1;
-        vector<int> res;
-        for(int i = 0;i<nums.size();i++){
-           if(nums[i]==0 ){
-            zero++;
-            
-           }
-           else{
-            prod = prod * nums[i];
-           }
+        for( int i = 0 ; i<n;i++){
+             if(nums[i]==0){
+                zeroes++;
+                
+             }
+             else{
+             prod = prod * nums[i];
+             }
+        } 
+        if(zeroes >=2){
+          for(int i = 0;i<n ; i++){
+            res[i] = 0;
+          }
         }
-        if(zero>1){
-           for(int i = 0;i<nums.size();i++){
-            res.push_back(0);
-           }
+        else if(zeroes == 1){
+            for(int i = 0;i<n;i++){
+                if(nums[i] == 0){
+                    res[i] = prod;
+                }
+                else{
+                    res[i] = 0;
+                }
+            }
         }
-        if(zero==1){
-           for(int i = 0;i<nums.size();i++){
-            if(nums[i] == 0){
-                res.push_back(prod);
-            }
-            else{
-                res.push_back(0);
-            }
-           }
-
-        }
-        if(zero == 0){
-            for(int i = 0;i< nums.size();i++){
-                res.push_back(prod/nums[i]);
-            }
+        else{
+            for(int i = 0;i<n ; i++){
+            res[i] = prod/nums[i];
+          }
         }
         return res;
     }
